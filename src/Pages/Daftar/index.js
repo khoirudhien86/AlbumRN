11009f416b419d6d69d9e9b0b1d6958051ef3920
khoirudhien86 <khoirudhien86.2@gmail.com>
2021-03-11 20:23:05 +0700

    memperbaiki code page login & register

--- src/Pages/Daftar/index.js
+++ src/Pages/Daftar/index.js
@@ -2,31 +2,27 @@ import React, { useState } from 'react'
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 
 const Daftar = () => {
-    const [value, onChangeText] = useState();
+    const [value, setDaftarEmail] = useState();
+    const [pasword, setDaftarPassword] = useState();
     return (
         <View>
             <Text style={{fontSize: 40, color: "#7B1FA2", fontWeight: 'bold', textAlign: 'center',marginTop: 20}}>MYAPP</Text>
             <TextInput 
                 style={{borderWidth: 1, borderColor: 'gray', margin: 20, height: 50, backgroundColor: 'gray', borderRadius: 20, color: 'white', paddingHorizontal: 20}}
-                onChangeText={text => onChangeText(text)}
-                value={value}
-                placeholder="masukkan nama lengkap anda"
-                placeholderTextColor="red"
-            />
-            <TextInput 
-                style={{borderWidth: 1, borderColor: 'gray', margin: 20, height: 50, backgroundColor: 'gray', borderRadius: 20, color: 'white', paddingHorizontal: 20}}
-                onChangeText={text => onChangeText(text)}
+                onChangeText={text => setDaftarEmail(text)}
                 value={value}
                 placeholder="masukkan email anda"
                 placeholderTextColor="red"
             />
             <TextInput 
                 style={{borderWidth: 1, borderColor: 'gray', margin: 20, height: 50, backgroundColor: 'gray', borderRadius: 20, color: 'white', paddingHorizontal: 20}}
+                onChangeText={text => setDaftarPassword(text)}
+                value={pasword}
                 placeholder="masukkan password anda"
                 placeholderTextColor="red"
             />
             <View style={{alignItems: 'center'}}>
-                <TouchableOpacity style={{borderColor: 'red', borderWidth: 1, marginTop: 10, borderRadius: 15, backgroundColor: 'yellow', width: 150}}>
+                <TouchableOpacity style={{borderColor: 'red', borderWidth: 1, marginTop: 10, borderRadius: 15, backgroundColor: '#E64A19', width: 150}}>
                         <Text style={{padding: 10, textAlign: 'center', justifyContent: 'center', fontSize: 20, fontWeight: 'bold', color: 'purple', textTransform: 'uppercase'}}>DAFTAR</Text>
                 </TouchableOpacity>
             </View>
